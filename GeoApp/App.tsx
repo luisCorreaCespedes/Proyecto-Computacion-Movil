53bfe78080cefdbe72a8cc906d1bf00b51714d31
Luis <luiscorreacespedes@gmail.com>
2022-12-02 20:26:21 -0300

    Added Icons

--- GeoApp/App.tsx
+++ GeoApp/App.tsx
@@ -1,69 +1,101 @@
 import * as React from 'react';
-import { View, Text , Image, ImageProps} from 'react-native';
-import { ImageSourcePropType } from 'react-native';
+import { View, Text , Image, ImageBackground, Button, Alert, TouchableOpacity} from 'react-native';
+import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
+/*ICONS*/
+import { FontAwesome } from '@expo/vector-icons';
 
+/*PAGES*/
 import { Home } from './pages/Home';
 import { Profile } from './pages/Profile';
 import { Other } from './pages/Other';
 
-import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import { TouchableOpacity } from 'react-native';
 
-
+/*Side Bar*/
 const Drawer = createDrawerNavigator();
 
 const CustomDrawer = (props: any) => {
   return (
     <View style={{flex: 1}}>
-
       <DrawerContentScrollView {...props}>
-        <View style={{
-            flexDirection: 'row', 
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            padding: 20,
-            backgroundColor: '#f6f6f6'
-          }}
-        >
-          <View>
-            <Text style={{ fontWeight: '800', fontSize: 18 }}>John Doe A.</Text>
-            <Text>example@email.com</Text>
-          </View>
-          <Image 
-            source={require('./assets/Profile.png')}
-            style={{
-              width: 80, 
-              height: 80, 
-              borderRadius: 15,
-              marginRight: 5,
-              marginBottom: 5
+        <ImageBackground source={require('./assets/menuBg.jpg')} style={{marginBottom: 15, opacity: 50}}>
+          <View style={{
+              flexDirection: 'row', 
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              padding: 20
             }}
-          />
-        </View>
+          >
+            <View>
+              <Text style={{ fontWeight: '800', fontSize: 18 }}>John Doe A.</Text>
+              <Text>example@email.com</Text>
+            </View>
+            <Image 
+              source={require('./assets/Profile.png')}
+              style={{
+                width: 80, 
+                height: 80, 
+                borderRadius: 15,
+                marginRight: 5,
+                marginBottom: 5
+              }}
+            />
+          </View>
+        </ImageBackground>
         <DrawerItemList {...props}/>
       </DrawerContentScrollView>
-      <TouchableOpacity style={{ 
-        position: 'absolute',
-        right: 0, 
-        left: 0,
-        bottom: 20,
-        backgroundColor: '#f6f6f6',
-        padding: 10
-        }}>
-        <Text style={{fontSize: 16, fontWeight: '800'}}> 🚪 Salir</Text>
-      </TouchableOpacity>
+      <View style={{
+        paddingLeft: 20, 
+        borderTopWidth:1,
+        borderTopColor: '#ccc'
+        }}
+      >
+        <TouchableOpacity onPress={()=>{}} style={{ paddingVertical: 15}}>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <FontAwesome name="sign-out" size={24} color="black" />
+            <Text style={{ fontWeight: '600', fontSize: 14, margin: 7 }}>Salir</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 const DrawerNavigator = () => {
   return(
-    <Drawer.Navigator drawerContent={(props) => <CustomDrawer {...props}/> }>
-      <Drawer.Screen component={Home} name=' 🏠 Inicio'/>
-      <Drawer.Screen component={Profile} name=' 👤 Perfil'/>
-      <Drawer.Screen component={Other} name=' 🐣 Otros'/>
+    <Drawer.Navigator 
+      screenOptions={{
+        headerShown: true, 
+        headerStyle: {
+          backgroundColor: 'transparent',
+          elevation: 0,
+          shadowOpacity: 0
+        },
+      }}
+      drawerContent={(props) => <CustomDrawer {...props}/>}
+    >
+      <Drawer.Screen component={Home} name='Inicio' options={{
+           title: 'Inicio',
+           drawerIcon: ({focused, size}) => (
+            <FontAwesome name="home" size={24} color="black" />
+           ),
+        }}
+      />
+      <Drawer.Screen component={Profile} name='Perfil' options={{
+           title: 'Perfil',
+           drawerIcon: ({focused, size}) => (
+            <FontAwesome name="users" size={23} color="black" />
+           ),
+        }}
+      />
+      <Drawer.Screen component={Other} name='Otros' options={{
+           title: 'Otros',
+           drawerIcon: ({focused, size}) => (
+            <FontAwesome name="leaf" size={24} color="black" />
+           ),
+        }}
+      />
     </Drawer.Navigator>
   );
 }
