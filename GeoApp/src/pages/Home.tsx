a5e5f9fe0a519d2d7c1d6d29d97469fd2ed5d309
aguirrerod <raguirre@andain.cl>
2022-12-15 14:55:39 -0300

    Add permissions to google maps location

--- GeoApp/src/pages/Home.tsx
+++ GeoApp/src/pages/Home.tsx
@@ -1,12 +1,50 @@
 /*Home View*/
-import React from "react";
+import * as React from "react";
+import * as Location from 'expo-location'
 import { Text, StyleSheet, View} from "react-native";
-import MapView from "react-native-maps";
+import MapView, {Marker} from "react-native-maps";
 
 const Home = () => {
+
+  const [origin, setOrigin] = React.useState({
+    latitude: -33.46604,
+    longitude: -70.59702,
+  })
+
+  React.useEffect(() => {
+    getLocationPermission();
+  }, [])
+
+async function getLocationPermission(){
+  let {status} = await Location.requestForegroundPermissionsAsync();
+  if (status !== 'granted'){
+    alert('Permiso denegado');
+    return;
+  }
+  let location = await Location.getCurrentPositionAsync({});
+  const current = {
+    latitude: location.coords.latitude,
+    longitude: location.coords.longitude
+  }
+  setOrigin(current)
+}
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} provider={'google'} showsUserLocation showsMyLocationButton />
+      <MapView 
+        style={styles.map} 
+        provider={'google'} 
+        showsUserLocation 
+        showsMyLocationButton 
+        initialRegion={{
+          latitude: origin.latitude,
+          longitude: origin.longitude,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.04,
+        }}
+      >
+        
+      </MapView>
     </View>
   );
 }
