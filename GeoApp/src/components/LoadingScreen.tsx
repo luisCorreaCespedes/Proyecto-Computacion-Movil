225370d5b36d8cc953b3bd545895ab702afbd524
Luis <luiscorreacespedes@gmail.com>
2022-12-15 00:00:59 -0300

    Update README.md

--- GeoApp/src/components/LoadingScreen.tsx
+++ GeoApp/src/components/LoadingScreen.tsx
@@ -2,8 +2,12 @@ import Spinner from 'react-native-loading-spinner-overlay'
 
 export const LoadingOverlay = ({loading}: {loading: boolean}) => {
     return <Spinner
-    visible= {loading}
-    textContent= 'wena choro'>
-
-    </Spinner>
+    visible = {loading}
+    overlayColor = "rgba(0, 0, 0, 0.60)"
+    textContent = 'Espere un momento...'
+    textStyle={{
+        color: '#FFF',
+        fontWeight: '400'
+    }}
+    />
 }
